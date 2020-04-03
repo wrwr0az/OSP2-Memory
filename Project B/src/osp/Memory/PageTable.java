b1c6d1bd62435b66e81fa669d474473f60bf3e8a
wrwr <wrwr@DESKTOP-07RHSUQ>
2020-04-03 21:29:47 +0300

    OSP2 Memory for EE-463 Course

---
+++ Project B/src/osp/Memory/PageTable.java
@@ -0,0 +1,96 @@
+package osp.Memory;
+/**
+    The PageTable class represents the page table for a given task.
+    A PageTable consists of an array of PageTableEntry objects.  This
+    page table is of the non-inverted type.
+
+    @OSPProject Memory
+*/
+import java.lang.Math;
+import osp.Tasks.*;
+import osp.Utilities.*;
+import osp.IFLModules.*;
+import osp.Hardware.*;
+
+public class PageTable extends IflPageTable
+{
+	int arraySize;
+    /**
+	   The page table constructor. Must call
+
+	       super(ownerTask)
+
+	   as its first statement. Then it must figure out
+	   what should be the size of a page table, and then
+	   create the page table, populating it with items of
+	   type, PageTableEntry.
+
+	   @OSPProject Memory
+    */
+    public PageTable(TaskCB ownerTask)
+    {
+        // your code goes here
+    	// call super
+    	super(ownerTask);
+    	// get the size of page table
+    	int size = MMU.getPageAddressBits();
+    	arraySize = (int) Math.pow(2, size);
+    	// create page table array
+    	pages = new PageTableEntry[arraySize];
+    	
+    	// initialize the pages
+    	for (int i = 0; i< pages.length; i++)
+    	pages[i] = new PageTableEntry(this, i);
+
+    }
+
+    /**
+       Frees up main memory occupied by the task.
+       Then unreserves the freed pages, if necessary.
+
+       @OSPProject Memory
+    */
+    public void do_deallocateMemory()
+    {
+        // your code goes here
+    	TaskCB task = getTask();
+    	
+    	for (int i=0; i< arraySize; i++) {
+    		
+    		
+    		
+    		FrameTableEntry frame = pages[i].getFrame();
+    		TaskCB frameTask = frame.getPage().getTask();
+    		if (frameTask == task && frame != null)
+    		{
+    			// nullify the page
+    			frame.setPage(null);
+    	    	// clean the page
+    			frame.setDirty(false);
+    	    	// unset the reference
+    			frame.setReferenced(false);
+    			
+    			// check if the task reserved a given frame then unreserves the freed pages 
+    			if(task == frame.getReserved())
+    				frame.setUnreserved(task);
+    		}
+    		
+    	}
+    	
+    	
+    	
+    	
+    	
+
+    }
+
+
+    /*
+       Feel free to add methods/fields to improve the readability of your code
+    */
+
+}
+
+/*
+      Feel free to add local classes to improve the readability of your code
+*/
---
+++ Project B/src/osp/Memory/PageTableEntry.java
@@ -0,0 +1,101 @@
+package osp.Memory;
+
+import osp.Hardware.*;
+import osp.Tasks.*;
+import osp.Threads.*;
+import osp.Devices.*;
+import osp.Utilities.*;
+import osp.IFLModules.*;
+
+/**
+ * The PageTableEntry object contains information about a specific virtual page
+ * in memory, including the page frame in which it resides.
+ * 
+ * @OSPProject Memory
+ */
+
+public class PageTableEntry extends IflPageTableEntry {
+	/**
+	 * The constructor. Must call
+	 * 
+	 * super(ownerPageTable,pageNumber);
+	 * 
+	 * as its first statement.
+	 * 
+	 * @OSPProject Memory
+	 */
+	public PageTableEntry(PageTable ownerPageTable, int pageNumber) {
+		// your code goes here
+		// calling super
+		super(ownerPageTable, pageNumber);
+
+	}
+
+	/**
+	 * This method increases the lock count on the page by one.
+	 * 
+	 * The method must FIRST increment lockCount, THEN check if the page is valid,
+	 * and if it is not and no page validation event is present for the page, start
+	 * page fault by calling PageFaultHandler.handlePageFault().
+	 * 
+	 * @return SUCCESS or FAILURE FAILURE happens when the pagefault due to locking
+	 *         fails or the that created the IORB thread gets killed.
+	 * 
+	 * @OSPProject Memory
+	 */
+	public int do_lock(IORB iorb) {
+		// your code goes here
+		// increment lockCount
+		getFrame().incrementLockCount();
+		// check if the page isn't valid
+		if (!isValid()) {
+			// check the validation event doesn't present
+			if (getValidatingThread() == null) {
+				// start page fault
+				int PFH = PageFaultHandler.handlePageFault(iorb.getThread(), MemoryLock, this);
+				// check if the pagefault fails
+				if (PFH == FAILURE || iorb.getThread().getStatus() == ThreadKill)
+					return FAILURE;
+			}
+
+			// check if the thread caused the page fault equal to this thread
+			else if (getValidatingThread() != iorb.getThread()) {
+
+				return FAILURE;
+
+			} else {
+				// suspend thread
+				iorb.getThread().suspend(this);
+			}
+
+		}
+
+		
+		return SUCCESS;
+
+	}
+
+	/**
+	 * This method decreases the lock count on the page by one.
+	 * 
+	 * This method must decrement lockCount, but not below zero.
+	 * 
+	 * @OSPProject Memory
+	 */
+	public void do_unlock() {
+		// your code goes here
+		// decrement lockCount if is not equal or less than 1
+		if (getFrame().getLockCount() > 0)
+			getFrame().decrementLockCount();
+
+	}
+
+	/*
+	 * Feel free to add methods/fields to improve the readability of your code
+	 */
+
+}
+
+/*
+ * Feel free to add local classes to improve the readability of your code
+ */
